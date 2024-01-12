e2284d56e9fc6251aec9ef46d65a1a6613325514
tonmai <71057039+ekgaparp@users.noreply.github.com>
2024-01-12 18:16:49 +0700

    add firebase

---
+++ lib/screen/home.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool value = false;
+  final db = FirebaseDatabase.instance.ref('Home');
+  onUpdate() {
+    setState(() {
+      value = !value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+          stream: db.child('Data').onValue,
+          builder: (context, snapshot) {
+            if (snapshot.hasData &&
+                !snapshot.hasError &&
+                snapshot.data != null) {
+              final data = snapshot.data!.snapshot.value;
+              print('======> data:$data');
+
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Temperature',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      // data['Humidity'],
+
+                      snapshot.data!.snapshot.value.toString(),
+                      style: const TextStyle(fontSize: 20.0),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                        onPressed: () {
+                          onUpdate();
+                          writeData();
+                        },
+                        child: Text(value ? 'ON' : 'OFF'))
+                  ],
+                ),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+
+  Future<void> writeData() async {
+    db.child('Data').set({'Humidity': 0, 'Temperature': 0});
+    db.child('LightState').set({'switch': !value});
+  }
+}
